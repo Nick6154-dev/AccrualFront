501c62e71aa8b4ec514ac34f92ede000859ddb3c
Nick6154-dev <pbalsecamh5b@outlook.com>
2023-02-25 08:18:10 -0500

    init

--- src/main.jsx
+++ src/main.jsx
@@ -61,8 +61,7 @@ const router = createBrowserRouter([
     }
 ])
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <RouterProvider router={router}/>
     </React.StrictMode>
